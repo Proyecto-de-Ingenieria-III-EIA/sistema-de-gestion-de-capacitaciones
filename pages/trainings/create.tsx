5edc3f70df2add252b0e767fc15dd18b027366cb
Maria <maria.meneses@eia.edu.co>
2025-04-10 00:18:00 -0500

    crear training

---
+++ pages/trainings/create.tsx
@@ -0,0 +1,14 @@
+"use client"
+
+
+import MainLayout from "@/components/layouts/main-layout"
+import CreateTraining from "@/components/templates/CreateTraining"
+import Home from "@/components/templates/home"
+
+export default function DashboardPage() {
+  return (
+    <MainLayout>
+      <CreateTraining/>
+    </MainLayout>
+  )
+}
